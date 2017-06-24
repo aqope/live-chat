acece30e0d83347bb259af74bae29984def23764
Artur Paklin <parkourtrane@gmail.com>
2017-06-24 22:33:38 +0300

    initial commit, working example from socket.io

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3000, function() {
+  log('listening on 3000 port');
+});
+
+app.get('/', function (request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+  log('User has connected');
+
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  })
+
+  socket.on('disconnect', function() {
+    log('Used has disconnected');
+  })
+});
+/*
+  Logs message to the console with timestamp
+*/
+function log(message) {
+  var date = new Date();
+  console.log(
+    date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " : " + message
+  );
+}
